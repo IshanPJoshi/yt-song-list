e60eafc6c06b27784370e1ba5c83205dd73b523d
joshi304 <55596316+joshi304@users.noreply.github.com>
2022-03-07 22:00:43 -0600

    setup db + housekeeping

--- schema.sql
+++ schema.sql
@@ -1,1 +1,9 @@
--- CREATE table IF NOT EXISTS --TABLE_NAME ();
+CREATE table IF NOT EXISTS youtube_songs (
+    song_id SERIAL PRIMARY KEY,
+    title text,
+    author text,
+    channel text,
+    link text,
+    thumbnail text,
+    date timestamp default NOW()
+);
